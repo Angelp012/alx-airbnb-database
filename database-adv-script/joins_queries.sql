4c473d053fa09a1d315ed1dd051f108afd8c20b4
Angel Praise <139116550+Angelp012@users.noreply.github.com>
2025-07-06 13:13:05 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,24 +1,16 @@
---LEFT JOIN
-SELECT * FROM Property
-LEFT JOIN Review
-ON Property.property_id = Review.property_id;
---INNER JOIN
-SELECT 
-    Booking.booking_id,
-    Booking.start_date,
-    Booking.end_date,
-    Booking.total_price,
-    User.first_name,
-    User.email
-FROM Booking
-INNER JOIN User ON Booking.user_id = User.user_id;
-Shows bookings and who made them
+-- INNER JOIN: All bookings and their respective users
+SELECT b.booking_id, b.start_date, b.end_date, u.first_name, u.last_name
+FROM Booking b
+INNER JOIN User u ON b.user_id = u.user_id;
 
---OUTER JOIN
-SELECT 
-    User.user_id,
-    User.first_name,
-    Booking.booking_id,
-    Booking.start_date
-FROM User
-FULL OUTER JOIN Booking ON User.user_id = Booking.user_id;
+-- LEFT JOIN: All properties and their reviews (including properties with no reviews)
+SELECT p.property_id, p.name AS property_name, r.rating, r.comment
+FROM Property p
+LEFT JOIN Review r ON p.property_id = r.property_id
+ORDER BY p.property_id;
+
+-- FULL OUTER JOIN: All users and all bookings, even if unlinked
+SELECT u.user_id, u.first_name, b.booking_id, b.start_date
+FROM User u
+FULL OUTER JOIN Booking b ON u.user_id = b.user_id
+ORDER BY u.user_id;
